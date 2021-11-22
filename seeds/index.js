b51b76f43396e8d7544afdea7c470b92a3b15638
Dominik Bogdanowicz <dominik.bogdanowicz.kontakt@gmail.com>
2021-11-22 13:55:35 +0100

    adding seeding logic

---
+++ seeds/index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, ("connection error:")));
+db.once("open", () => {
+    console.log('Database connected');
+});
+
+const seedDb = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        new Campground({
+            location: `${cities[random1000].city}, ${cities.[random1000].state}`
+        })
+        await camp.save();
+    }
+}
+
+seedDb();
