19f6b1f59d02eeb7fe2e8d668db7bee866a6597e
Dominik Bogdanowicz <dominik.bogdanowicz.kontakt@gmail.com>
2021-11-22 13:57:58 +0100

    removing syntax error in seeding logic

--- seeds/index.js
+++ seeds/index.js
@@ -17,7 +17,7 @@ const seedDb = async () => {
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         new Campground({
-            location: `${cities[random1000].city}, ${cities.[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`
         })
         await camp.save();
     }
