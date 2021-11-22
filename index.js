cb70875397ba0884d51145e6008f7e1e33229304
Dominik Bogdanowicz <dominik.bogdanowicz.kontakt@gmail.com>
2021-11-22 14:58:47 +0100

    adding camp detail by id route

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 })
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground });
+})
+
 app.listen(3000, () => {
     console.log('On port 3000');
 })
