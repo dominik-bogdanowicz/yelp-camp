13dedbbcc175ced152c1c7aeb3c631a979835730
Dominik Bogdanowicz <dominik.bogdanowicz.kontakt@gmail.com>
2021-11-22 13:41:09 +0100

    adding get route for adding camps to database

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true
 });
 
@@ -22,6 +22,15 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({
+        title: 'My backyard',
+        description: 'Cheap camping'
+    });
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {
