7da39d39d8430e715bb6d06b9d68ba60a186a4de
Dominik Bogdanowicz <dominik.bogdanowicz.kontakt@gmail.com>
2021-11-25 12:51:10 +0100

    adding bootstrap styling to all pages and updating model

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDb = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const randomPrice = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti possimus excepturi architecto mollitia maxime eveniet doloremque unde odio, expedita asperiores, recusandae consequatur iusto minima fugit, et eos laudantium. Veritatis, aut!',
+            price: randomPrice
         })
         await camp.save();
     }
