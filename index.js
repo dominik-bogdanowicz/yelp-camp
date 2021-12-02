32889130a71fd1ac0980f735510a13cdf3cec8e5
Dominik Bogdanowicz <dominik.bogdanowicz.kontakt@gmail.com>
2021-12-02 14:09:59 +0100

    adding session

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const Review = require('./models/review');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const campgroundJoiSchema = require('./utils/campgroundJoiSchema');
-const reviewJoiSchema = require('./utils/reviewJoiSchema');
+const session = require('express-session');
 
 const campgrounds = require('./routes/campground');
 const reviews = require('./routes/reviews')
@@ -28,11 +25,19 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', ejsMate);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
 app.use(express.static('public'));
+
+const sessionConfig = {
+    secret: 'thisWillBeSecret',
+    resave: false,
+    saveUninitialized: true
+}
+app.use(session(sessionConfig));
 
 //Generic 404 for every not specified url
 app.all('*', (req, res, next) => {
