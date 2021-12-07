5be2c8d3ad8c09331a12a2eb805f1eba28300645
Dominik Bogdanowicz <dominik.bogdanowicz.kontakt@gmail.com>
2021-12-07 14:30:14 +0100

    addig login route and styling

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -14,4 +15,12 @@ router.post('/register', catchAsync(async (req, res) => {
     req.flash('success', 'Welcome to Yelp Camp!');
     res.redirect('/campgrounds');
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/campgrounds');
+});
 module.exports = router;
